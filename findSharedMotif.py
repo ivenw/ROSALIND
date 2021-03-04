bfe5c2ac8930328ddc1ae111687e570e9d4e1490
Iven Winkelmann <ivenwinkelmann@Ivens-MacBook-Pro.local>
2021-03-04 16:22:58 +0100

    solution for find shared motif problem

--- findSharedMotif.py
+++ findSharedMotif.py
@@ -3,8 +3,26 @@ from fastaImport import *
 def findSharedMotif(seq_dict) :
 	'''takes dict with sequences as input and returns a longest common substring of the collection'''
 
+	seq_list = list( seq_dict.values() )
 
+	seq_list = sorted(seq_list, key=len)
 
-file = '/Users/ivenwinkelmann/Downloads/rosalind_subs.txt'
+	shortest_seq = seq_list.pop(0)
+
+	shortest_seq_len = len(shortest_seq)
+
+	for chunk_size in range( shortest_seq_len, 0, -1 ) :
+		for pos in range(0, shortest_seq_len - chunk_size + 1) :
+			count = len(seq_list)
+			chunk = shortest_seq[pos:pos + chunk_size]
+			for seq in seq_list :
+				if chunk not in seq :
+					break
+				else :
+					count -= 1
+			if count == 0 :
+				return chunk
+
+file = 'test_input.txt'
 
 print(findSharedMotif(fastaImport(file)))
