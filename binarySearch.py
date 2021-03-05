29e235d3dca23117c9b0fc99609917e8d1f6c7a6
Iven Winkelmann <ivenwinkelmann@Ivens-MacBook-Pro.local>
2021-03-05 13:16:20 +0100

    Update binarySearch.py

--- binarySearch.py
+++ binarySearch.py
@@ -1,3 +1,5 @@
+import math
+
 def binarySearch(list, length, element) :
 
 	'''Python implementation of binary search algorithm.
@@ -5,14 +7,19 @@ def binarySearch(list, length, element) :
 	Returns the index of element in list. If the element can't be found returns None.'''
 
 	left = 0		#init left boundary
-	right = length - 1		#init right boundary. -1 because of list indexing
+	right = length + 1		#init right boundary. -1 because of list indexing
 
-	while left < right :
-		mid = floor((right - mid) / 2)
+	while left <= right :
+		mid = math.floor((right + left) / 2)
 		if list[mid] < element :
 			left = mid + 1
 		elif list[mid] > element :
-			right = mid 1
+			right = mid - 1
 		else:
-			return mid
+			return mid - 1
 	return None
+
+test_list = sorted([5,3,2,5,3,3,2,7,5,4,4,6,2,9,8,7])
+
+print(test_list)
+print(binarySearch(test_list, len(test_list), 9))
